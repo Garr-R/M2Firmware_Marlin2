4c5b0a463a10d9d92d49cdbb4f8adc185f8e38d1
Garrett Broters <52166834+Garr-R@users.noreply.github.com>
2023-06-16 16:01:11 -0400

    fixed Zmin position

--- Firmware/Configuration/Configuration.h
+++ Firmware/Configuration/Configuration.h
@@ -1700,7 +1700,7 @@
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS -4
 #define Y_MIN_POS -2
-#define Z_MIN_POS -15
+#define Z_MIN_POS  0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 200
